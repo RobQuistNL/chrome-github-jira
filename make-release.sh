755a08d4cecb2cd2868d473bcd382a350543b4d5
RobQuistNL <rob.quist@dukesoft.nl>
2026-02-26 18:00:47 +0100

    Remove changelog from zip

--- make-release.sh
+++ make-release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 VERSION=1.3.1
 rm chrome-github-jira-v$VERSION.zip
-zip -n9 chrome-github-jira-v$VERSION.zip images/* src/* CHANGELOG.md LICENSE manifest.json PRIVACY.md README.md
+zip -n9 chrome-github-jira-v$VERSION.zip images/* src/* LICENSE manifest.json PRIVACY.md README.md
